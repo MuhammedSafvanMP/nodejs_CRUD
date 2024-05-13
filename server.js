b375b952b605bffa9a57baa94f44a4a8dec5c1ef
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2024-05-13 20:26:18 +0530

    created user.json data get that data

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const http = require('http');
 const dotenv = require("dotenv");
 dotenv.config();
 
+const user = require("./db/user.json");
+
+
 const PORT = process.env.PORT || 8080;
 
 http.createServer( (req, res) => {
     if (req.method === "GET") {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Hello World!');
+        res.writeHead(200, {'Content-Type': 'text/json'});
+
+        
+        res.write(JSON.stringify(user));
         res.end();
     }
 }).listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
